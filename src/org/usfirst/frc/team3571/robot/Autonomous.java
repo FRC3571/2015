ebd90343056e1fb8c8efef09f9c57fcad7500cf4
TomasUB <rakusan2@gmail.com>
2015-03-25 14:46:13 -0400

    Basic autonomous

--- src/org/usfirst/frc/team3571/robot/Autonomous.java
+++ src/org/usfirst/frc/team3571/robot/Autonomous.java
@@ -1,16 +1,22 @@
 package org.usfirst.frc.team3571.robot;
+import edu.wpi.first.wpilibj.Timer;
 
 public class Autonomous {
 
 	/**
 	 * Autonomous initialization code
 	 */
+	public static Timer t1=new Timer();
  public static void AutoInit(){
+	 t1.reset();
+	 t1.start();
  }
+ 	
 	/**
 	 * Autonomous periodic code
 	 */
  public static void AutoP(){
-	 
+	 double time=t1.get();
+	 if(time<2)Global.ArcadeDrive(0, 0.5, 0);
  }
 }
